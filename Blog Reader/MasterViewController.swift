de4f585d7c3f19ca874d002c5e78a499b3363a85
master <libaoming2@gmail.com>
2016-08-26 12:33:26 +0800

    google blogger api demo

--- Blog Reader/MasterViewController.swift
+++ Blog Reader/MasterViewController.swift
@@ -17,21 +17,125 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem
+       
+        if  let url = URL(string: "https://www.googleapis.com/blogger/v3/blogs/10861780/posts?key=AIzaSyDlAlcNLvTsD2hM6VgMIK_I5wdzBj7qfMk") {
+        
+        let task = URLSession.shared.dataTask(with: url){
+            (data,response,error) in
+            
+            if error != nil {
+                print("xxxxxxxxxxxxxxxxxxx")
+                print("xxxxxxxxxxxxxxxxxxx")
+                print(error)
+                print("xxxxxxxxxxxxxxxxxxx")
+                print("xxxxxxxxxxxxxxxxxxx")
+            }else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                    
+                    let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as? AnyObject
+                    
+                    
+                    
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+                        
+                        if let items = jsonResult?["items"] as? NSArray {
+                            
+                            let context = self.fetchedResultsController.managedObjectContext
+                            
+                            let request = NSFetchRequest<Event>(entityName: "Event")
+
+                            do {
+                                
+                                let results = try context.fetch(request)
+                                
+                                if results.count > 0 {
+                                    
+                                    for result in results {
+                                        
+                                        do {
+                                            
+                                            context.delete(result)
+                                           try  context.save()
+                                            
+                                        }catch {
+                                            
+                                        }
+                                    }
+                                }
+                                
+                            } catch {
+                                
+                            }
+                            
+                            for item in items {
+                                
+                                let itemTemp = item as? AnyObject
+                                
+                                print(itemTemp?["published"] )
+                                print(itemTemp?["title"])
+                                
+                                let newEvent = Event(context: context)
+                                
+                                // If appropriate, configure the new managed object.
+                                newEvent.timestamp = NSDate()
+                                newEvent.setValue(itemTemp?["title"] as! String, forKey: "title")
+                                newEvent.setValue(itemTemp?["content"] as! String, forKey: "content")
+                                newEvent.setValue(itemTemp?["published"] as! String, forKey: "published")
+                                
+                                
+                                // Save the context.
+                                do {
+                                    try context.save()
+                                } catch {
+                                    // Replace this implementation with code to handle the error appropriately.
+                                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                                    let nserror = error as NSError
+                                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                                }
+
+                                
+                                
+                                
+                            }
+                            
+                            
+                        }
+                    } catch {
+                        
+                        print("xxxxxxxxxxxxxxxxxxx")
+                        print("xxxxxxxxxxxxxxxxxxx")
+                        
+                        print("fail to convert from json")
+                        print("xxxxxxxxxxxxxxxxxxx")
+                        print("xxxxxxxxxxxxxxxxxxx")
+                    }
+                    
+                }
+            }
         }
+        
+        task.resume()
+        
+        } else {
+            
+            print("xxxxxxxxxxxxxxxxxxx")
+            print("xxxxxxxxxxxxxxxxxxx")
+            
+            print("fuck GFW.......")
+            print("xxxxxxxxxxxxxxxxxxx")
+            print("xxxxxxxxxxxxxxxxxxx")
+
+        }
+        
+        
+        self.tableView.reloadData()
+        
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        self.clearsSelectionOnViewWillAppear = self.splitViewController!.isCollapsed
-        super.viewWillAppear(animated)
-    }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -90,27 +194,12 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let context = self.fetchedResultsController.managedObjectContext
-            context.delete(self.fetchedResultsController.object(at: indexPath))
-                
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-
+   
     func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
-        cell.textLabel!.text = event.timestamp!.description
+        cell.textLabel!.text = event.value(forKey: "title") as? String
     }
 
     // MARK: - Fetched results controller
@@ -126,7 +215,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "published", ascending: false)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
